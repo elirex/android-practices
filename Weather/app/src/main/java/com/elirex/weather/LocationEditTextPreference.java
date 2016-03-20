14c894e7e86ba94454f09612bf3504b948cab242
Elirex <sheng79225@gmail.com>
2016-03-20 23:23:49 +0800

    Weather - Fix the custom attribute error

--- Weather/app/src/main/java/com/elirex/weather/LocationEditTextPreference.java
+++ Weather/app/src/main/java/com/elirex/weather/LocationEditTextPreference.java
@@ -1,9 +1,16 @@
 package com.elirex.weather;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Wang, Sheng-Yuan (Elirex) on 2016/3/20.
@@ -12,18 +19,49 @@ public class LocationEditTextPreference extends EditTextPreference {
 
     private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
 
-    private int mMinLegth;
+    private int mMinLength;
 
     public LocationEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(
                 attrs, R.styleable.LocationEditTextPreference, 0, 0);
         try {
-            mMinLegth = typedArray.getInteger(R.styleable.LocationEditTextPreference_minLenght,
+            mMinLength = typedArray.getInteger(R.styleable.LocationEditTextPreference_minLength,
                     DEFAULT_MINIMUM_LOCATION_LENGTH);
         } finally {
             typedArray.recycle();
         }
     }
 
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        EditText editText = getEditText();
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog dialog = getDialog();
+                if( dialog instanceof AlertDialog) {
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    if(s.length() < mMinLength) {
+                        positiveButton.setEnabled(false);
+                    } else {
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
 }
